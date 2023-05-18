ac75df338f0636489bba1fdcdbe7057f526505c1
mtoni1 <michellewambaya2@gmail.com>
2023-05-18 23:14:42 +0300

    c basic programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+*main - print the string in the root function
+*
+*Description: using the main function
+*this program prints with proper grammar, but the outcome is a piece of art'
+*Return 0
+
+*/
+int main(void)
+(
+        printf("with proper grammar, but the outcome is a piece of art,/n");
+        return (0);
+)

303549fbc13ccc90a07be854eb9530e6236e6b42
mtoni1 <michellewambaya2@gmail.com>
2023-05-22 20:28:46 -0700

    0x00-hello_world project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*main - C program that prints the size of various types
-*warnings are allowed*
-
+*main - print the string in the put function
+*
+*Description: using the main function
+*this program prints 'sizes of data types'
 *Return: 0
 */
 int main(void)
@@ -12,10 +13,10 @@ int i;
 long li;
 long long lli;
 float f;
-printf(size of a char: %ld byte(s)\n", sizeof(c));
-printf(size of a int: %ld byte(s)\n", sizeof(i));
-printf(size of a long: %ld byte(s)\n", sizeof(li));
-printf(size of a long long: %ld byte(s)\n", sizeof(lli));
-printf(size of a float: %ld byte(s)\n", sizeof(f));
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of a int: %ld byte(s)\n", sizeof(i));
+printf("size of a long: %ld byte(s)\n", sizeof(li));
+printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
 return(0);
 )

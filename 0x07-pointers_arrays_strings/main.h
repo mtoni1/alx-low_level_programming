91d22d1bb5053dd500495538b38797ddf01e17b0
mtoni1 <michellewambaya2@gmail.com>
2024-01-08 12:39:26 -0800

    Tasks

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,21 +14,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif
-
-
-
-_putchar.c
-
-#include <unistd.h>
-
-/**
-   * _putchar - writes the character c to stdout
-    * @c: The character to print
-     *
-      * Return: On success 1.
-       * On error, -1 is returned, and errno is set appropriately.
-        */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}

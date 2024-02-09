8202e22aa4c8c85d1ebe95c390014478144584a2
mtoni1 <michellewambaya2@gmail.com>
2024-02-09 11:32:11 -0800

    Tasks

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Function - converts a binary number to an unsigned int
- * @b: pointer to a string containing a binary number
+ * binary_to_uint - a function that converts a binary number to an unsigned int
+ * @b: pointer to a string containing a binary number a string of 0 and 1 chars
  *
- * Return: the converted number, or 0 if there is one or more chars in the string b that is not 0 or 1,b is NULL
- *
+ * Return:  the converted number, or 0
  */
-
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned int binary_to_uint(const char *b) {
-
-   int a;
+	int a;
 	unsigned int num;
 
 	num = 0;
@@ -31,5 +26,4 @@ int main(void)
 			num += 1;
 	}
 	return (num);
-
 }

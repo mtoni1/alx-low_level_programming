da519d83d99e48f1f9b8e366ecc319bbf33381b3
mtoni1 <michellewambaya2@gmail.com>
2024-02-09 11:27:20 -0800

    Tasks

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,35 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
 
-int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+/**
+ * main - Entry point
+ * Function - converts a binary number to an unsigned int
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: the converted number, or 0 if there is one or more chars in the string b that is not 0 or 1,b is NULL
+ *
+ */
 
-#endif
+int main(void)
+{
+    unsigned int binary_to_uint(const char *b) {
+
+   int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
+
+}

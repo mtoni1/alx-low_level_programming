2d57f0b28f561fcc3bf0d869b8324253c64bd280
mtoni1 <michellewambaya2@gmail.com>
2024-02-15 02:35:27 -0800

    Tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include "main.h"
-
-
-/**
- * read_textfile -  function that reads a text file and prints it to the POSIX standard output
- * @n: number to print, if the file can not be opened or read, return 0(NULL)
- *
- * Return: 1(on success)
- */
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int create_file(const char *filename, char *text_content) {
     if (filename == NULL) {
         return -1;
     }
-
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1) {
-        perror("Error: Can't create file");
         return -1;
     }
-
     if (text_content != NULL) {
         size_t len = strlen(text_content);
-        if (write(fd, text_content, len) != len) {
-            perror("Error: Can't write to file");
+        ssize_t bytes_written = write(fd, text_content, len);
+        if (bytes_written == -1 || (size_t)bytes_written != len) {
             close(fd);
             return -1;
         }
     }
-
-    if (close(fd) == -1) {
-        perror("Error: Can't close file");
-        return -1;
-    }
-
+    close(fd);
     return 1;
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,39 +1,29 @@
-#ifndef ELF_UTILS_H
-#define ELF_UTILS_H
+#ifndef CREATE_FILE_H
+#define CREATE_FILE_H
 
-#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <fcntl.h> // For O_APPEND
-#include <unistd.h> // For close()
-#include <errno.h>
+#include <sys/types.h>
 
-
-
-#define EI_NIDENT 16
-#define BUFFER_SIZE 1024
-
-typedef struct {
-    unsigned char e_ident[EI_NIDENT];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint64_t e_entry;
-    uint64_t e_phoff;
-    uint64_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
-} Elf64_Ehdr;
-
-void print_elf_header(const Elf64_Ehdr *header);
-int copy_file(const char *src_filename, const char *dest_filename);
-int append_text_to_file(const char *filename, char *text_content);
+/**
+ * @brief Creates a file with the given filename and writes the given text content to it.
+ *
+ * @param filename The name of the file to create.
+ * @param text_content A NULL terminated string to write to the file.
+ *
+ * @return 1 on success, -1 on failure (file can not be created, file can not be written, write "fails", etc.).
+ *
+ * @note The created file must have permissions rw-------. If the file already exists, its contents are truncated.
+ *
+ * @note If filename is NULL, the function returns -1.
+ *
+ * @note If text_content is NULL, an empty file is created.
+ *
+ * @note ltrace allowed functions: write, printf, strlen, putchar, puts, malloc, free, exit, open, read, write, close
+ */
 int create_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
 
-#endif /* ELF_UTILS_H */
+#endif 

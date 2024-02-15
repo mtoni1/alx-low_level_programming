1c967742ddb51dc6423b977865f30fa45d5bc15f
mtoni1 <michellewambaya2@gmail.com>
2024-02-15 02:19:03 -0800

    Tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 #include "main.h"
 
 
 /**
- * create_file -  function that creates a file
- * @c: where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
- * 
- * Returns: 1 on success, -1 on failure
+ * read_textfile -  function that reads a text file and prints it to the POSIX standard output
+ * @n: number to print, if the file can not be opened or read, return 0(NULL)
+ *
+ * Return: 1(on success)
  */
-
-
 
 int create_file(const char *filename, char *text_content) {
     if (filename == NULL) {
         return -1;
     }
 
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL) {
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd == -1) {
+        perror("Error: Can't create file");
         return -1;
     }
 
     if (text_content != NULL) {
         size_t len = strlen(text_content);
-        ssize_t bytes_written = fwrite(text_content, sizeof(char), len, fp);
-        if (bytes_written == -1) {
-            fclose(fp);
+        if (write(fd, text_content, len) != len) {
+            perror("Error: Can't write to file");
+            close(fd);
             return -1;
         }
     }
 
-    fclose(fp);
-
-    struct stat st;
-    if (stat(filename, &st) == 0) {
-        chmod(filename, S_IRUSR | S_IWUSR);
+    if (close(fd) == -1) {
+        perror("Error: Can't close file");
+        return -1;
     }
 
     return 1;

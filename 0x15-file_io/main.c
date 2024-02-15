bdb1514ef121819efe32f3bfd86ff6a9ac080547
mtoni1 <michellewambaya2@gmail.com>
2024-02-15 02:56:15 -0800

    Hot fix

---
+++ 0x15-file_io/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "create_file.h"
+
+int main(void) {
+    int rc;
+    const char *filename = "test_file.txt";
+    const char *text_content = "Hello World!\n";
+
+    rc = create_file(filename, text_content);
+    if (rc != 0) {
+        fprintf(stderr, "create_file() failed: %d\n", rc);
+        return 1;
+    }
+
+    printf("File '%s' created successfully.\n", filename);
+
+    return 0;
+}

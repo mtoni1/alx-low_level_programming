605b41500abc2464c88bbf5f732508c1610a6d8f
mtoni1 <michellewambaya2@gmail.com>
2024-02-17 00:09:14 -0800

    Corrections

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,22 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdio.h>
-
+/**
+ * @brief Single linked list node
+ *
+ * @tparam T Type of data stored in the list
+ */
 typedef struct list_s {
-
+    T data;
+    struct list_s *next;
 } list_t;
 
+/**
+ * @brief Print all the elements of a list
+ *
+ * @param h Head of the list
+ * @return The number of nodes
+ */
 size_t print_list(const list_t *h);
 
-#endif 
+#endif

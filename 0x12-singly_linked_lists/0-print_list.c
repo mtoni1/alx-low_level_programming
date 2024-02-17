68a67f60bc7cf96971b9024ab1f29e0beb2a3f1c
mtoni1 <michellewambaya2@gmail.com>
2024-02-16 23:51:05 -0800

    Corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,20 @@
-#include <stdlib.h>
-#include <string.h>
+#include "list.h"
 #include <stdio.h>
-#include "lists.h"
-
 
 /**
-* print_list - check the code for Holberton School students.
-* @h: name of the list
-* Return: the number of nodes.
-*/
+ * print_list - Print all the elements of a list
+ * @h: A pointer to the head of the list
+ *
+ * Return: The number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-   int count = 0;
-
-
-   while (h)
-   {
-       if (h->str == NULL)
-       {
-           printf("[0] (nil)\n");
-       }
-       else
-       {
-       printf("[%d] %s\n", h->len, h->str);
-       }
-       count++;
-       h = h->next;
-   }
-   return (count);
+    size_t count = 0;
+    /* Iterate through the list and print each element */
+    for (; h != NULL; h = h->next)
+    {
+        printf("%s\n", h->str); /* Assuming the list contains strings */
+        count++;
+    }
+    return count;
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,23 +1,10 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+typedef struct list_s {
 
-typedef struct list_t {
-    int n;
-    struct list_t *next;
 } list_t;
+
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-int _strlen(const char *s);
-list_t *add_node(list_t **head, const char *str);
-unsigned int _strlen(char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-void myStartupFun(void) __attribute__((constructor));
-void myStartupFun(void);
 
-
-#endif
+#endif 
